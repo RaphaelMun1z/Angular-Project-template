4c536379970d9f5c4e13f27bde6cf55e81127fe6
RaphaelMun1z <raphaelmunizvarela@hotmail.com>
2025-01-17 19:21:30 -0300

    feat: template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { ButtonModule } from 'primeng/button';
+import { StepperModule } from 'primeng/stepper';
+
+interface AutoCompleteCompleteEvent {
+    originalEvent: Event;
+    query: string;
+}
+
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    imports: [RouterOutlet, ButtonModule, StepperModule],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
-  title = 'lease-insight';
+    title = 'lease-insight';
 }
